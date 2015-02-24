cbc915710e7b49f130968fc35f25dcd547be1469
ieshua <ieshua529@gmail.com>
2015-02-24 22:51:09 +0300

    наброски. пишите, как на ваш счёт должен выглядеть интерфейс (только без фанатизма).

---
+++ et-java/app-gui/src/main/java/com/company/et/gui/MainSceneController.java
@@ -0,0 +1,33 @@
+package com.company.et.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import com.company.et.domain.Professor;
+
+
+
+public class MainSceneController implements Initializable {
+    
+    
+    
+    
+    @FXML
+    private void newProfessorHandler(ActionEvent event) {
+       
+        
+    }
+    @FXML
+    private void removeProfessorHandler(ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
--- et-java/app-main/src/main/java/com/company/et/main/MainApp.java
+++ et-java/app-main/src/main/java/com/company/et/main/MainApp.java
@@ -1,15 +1,59 @@
 package com.company.et.main;
 
+//
+//import com.company.et.domain.Professor;
+//import com.company.et.domain.Task;
+//import com.company.et.service.ProfessorService;
+//import com.company.et.service.JsonService;
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+//public class MainApp {
+//
+//
+//    /**
+//     * The main() method is ignored in correctly deployed JavaFX application.
+//     * main() serves only as fallback in case the application can not be
+//     * launched through deployment artifacts, e.g., in IDEs with limited FX
+//     * support. NetBeans ignores main().
+//     *
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) throws IOException, ParseException {
+//        Professor professor = new Professor();
+//        //test example
+//        ProfessorService.addTaskToProfessor(professor, new Task("dfsdfadfad"));
+//        System.out.println(professor.getProfessorTasks().get(0));
+//        
+//        System.out.println(JsonService.objectToString(professor));
+//        
+//    }
+//
+//}
 
-import com.company.et.domain.Professor;
-import com.company.et.domain.Task;
-import com.company.et.service.ProfessorService;
-import com.company.et.service.JsonService;
-import java.io.IOException;
-import java.text.ParseException;
 
-public class MainApp {
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainScene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("Шишак Пальчука");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
@@ -19,14 +63,8 @@ public class MainApp {
      *
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException, ParseException {
-        Professor professor = new Professor();
-        //test example
-        ProfessorService.addTaskToProfessor(professor, new Task("dfsdfadfad"));
-        System.out.println(professor.getProfessorTasks().get(0));
-        
-        System.out.println(JsonService.objectToString(professor));
-        
+    public static void main(String[] args) {
+        launch(args);
     }
 
 }
