d9a347387628f3c945729d9e0045ca27f3073146
ieshua <ieshua529@gmail.com>
2015-02-25 12:47:03 +0300

    хунта всякая

--- et-java/app-gui/src/main/java/com/company/et/gui/MainSceneController.java
+++ et-java/app-gui/src/main/java/com/company/et/gui/MainSceneController.java
@@ -8,14 +8,26 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import com.company.et.domain.Professor;
+import com.company.et.domain.Task;
+import javafx.scene.control.TableView;
 
 
 
 public class MainSceneController implements Initializable {
     
+    @FXML TableView<Task> tableOfTasks;
     
+    //MenuBar
+    @FXML
+    private void fileExit(ActionEvent event) {
+        
+    }
     
+    @FXML private void fileOpen(ActionEvent event) {
+        
+    }
     
+    //Buttons
     @FXML
     private void newProfessorHandler(ActionEvent event) {
        
@@ -29,5 +41,6 @@ public class MainSceneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
 }
